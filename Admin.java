b05595f7154b815a962df2d3876ce9b1c17cdda7
Md. Nazmus Shakib Khan <105985762+Nazmus998@users.noreply.github.com>
2023-11-30 12:40:00 +0600

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Admin {
+	 Scanner scanner = new Scanner(System.in);
+	 
+	
+	void acc()
+	{
+		
+		
+		
+			System.out.println("\t\t\t1.View Ticket Details\n\t\t\t2.Tottal Ticket\n\t\t\t3.Edit Movie List\n\t\t\t4.Exit\n");
+			int ch = scanner.nextInt();
+			if(ch==1)
+			{
+				System.out.println("\t\t\t\t\t***AVAILABLE SOON***\t\t\t");
+			}
+			if(ch==2)
+			{
+				System.out.println("\t\t\t\t\t***AVAILABLE SOON***\t\t\t");
+			}
+			if(ch==3)
+			{
+				System.out.println("\t\t\t\t\t***AVAILABLE SOON***\t\t\t");
+			}
+			if(ch==4)
+			{
+				System.out.println("\t\t\t\t\t***Good Luck!***\t\t\t");
+				return;
+			}
+
+		
+		
+	
+	}
+
+}
