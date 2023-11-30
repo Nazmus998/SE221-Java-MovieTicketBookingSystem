2a0066a8f1b287409f0072e979de2696df92449d
Md. Nazmus Shakib Khan <105985762+Nazmus998@users.noreply.github.com>
2023-11-30 12:42:00 +0600

    Create Payment.java

---
+++ Payment.java
@@ -0,0 +1,13 @@
+public class Payment {
+    private int amouont;
+    private int otp=998;
+    void setAmouont(int wn){
+        this.amouont=400*wn;
+    }
+    int getAmouont(){
+        return amouont;
+    }
+    int getOtp(){
+        return otp;
+    }
+}
